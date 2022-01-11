906eefda20909dc7395662ca9599649ba8fe4f05
Lack1ce <hl.zheng@comsand.com>
2022-01-11 22:09:13 +0800

    style:home page

---
+++ src/components/navTab.vue
@@ -0,0 +1,175 @@
+<template>
+<div class="container">
+  <div class="content">
+    <div class="content-left">
+      <div class="content-left-logo">Logo</div>
+      <div class="content-left-tabs">
+        <div class="content-left-tabs-item" @click="clickTabsItem(item, index)" :class="index === currentTab ? 'content-left-tabs-item-clicked' : ''" v-for="(item, index) in tabsItemList" :key="index">
+          {{ item.title }}
+        </div>
+      </div>
+    </div>
+    <div class="content-right">
+      <div class="content-right-search">
+        <div class="content-right-search-button">
+          <el-select v-model="value" placeholder="Please choose">
+            <el-option
+              v-for="item in options"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value">
+            </el-option>
+          </el-select>
+        </div>
+        <div class="content-right-search-input">
+          <input type="text" placeholder="Enter Search Value">
+        </div>
+        <div class="content-right-search-icon icon iconfont icon-search1"></div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+import {
+  select,option
+} from 'element-plus'
+export default {
+  components: { select, option},
+  data() {
+    return {
+      tabsItemList: [{
+        title: 'Dashboard'
+      }, {
+        title: 'Staking'
+      }, {
+        title: 'Transactions'
+      }, {
+        title: 'Assets'
+      }, {
+        title: 'Gov'
+      }],
+      currentTab: 0,
+      options: [{
+          value: '选项1',
+          label: '选项1'
+        }, {
+          value: '选项2',
+          label: '选项2'
+        }, {
+          value: '选项3',
+          label: '选项3'
+        }, {
+          value: '选项4',
+          label: '选项4'
+        }, {
+          value: '选项5',
+          label: '选项5'
+        }],
+        value: ''
+    }
+  },
+  methods: {
+    clickTabsItem(item, index) {
+      this.currentTab = index
+      this.emit('clickTabsItem', this.currentTab)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  position: sticky;
+  top: 0;
+  width: 100%;
+  background: #424958;
+  display: flex;
+  justify-content: center;
+  padding: 20px 0;
+  z-index: 999;
+
+  .content {
+    display: flex;
+    width: 80%;
+    margin: 0 auto;
+    justify-content: space-between;
+    align-items: center;
+
+    &-left {
+      color: #fff;
+      display: flex;
+      align-items: center;
+
+      &-logo {
+        // width: 183px;
+
+      }
+
+      &-tabs {
+        display: flex;
+        margin-top: 10px;
+        justify-content: flex-start;
+        align-items: center;
+        margin-left: 30px;
+
+        &-item {
+          margin-right: 30px;
+          padding-bottom: 10px;
+          cursor: pointer;
+          font-weight: 400;
+        }
+
+        &-item-clicked {
+          border-bottom: 4px solid #fff;
+          font-weight: 700;
+        }
+      }
+
+    }
+    &-right {
+      flex: 1;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      padding: 2px;
+      background: #4e5f72;
+      border-radius: 2px;
+      color: #fff;
+      &-search {
+        width: 100%;
+        display: flex;
+        border: 1px solid #5872a9;
+        align-items: center;
+        justify-content: space-around;
+        &-button {
+          padding-left: 10px;
+          height: 40px;
+          line-height: 40px;
+      }
+      &-input {
+        width: 70%;
+        input {
+          background: #4e5f72;
+          border: none;
+          color: #fff;
+          text-indent: 20px;
+          width: 100%;
+          outline: none;
+        }
+        input::placeholder {
+              color: #fff;
+            }
+      }
+      &-icon {
+        padding-right: 10px;
+      }
+      }
+    }
+  }
+}
+.el-input {
+  background: red;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,287 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="home">
+  <nav-tab @clickTabsItem="clickTabsItem"></nav-tab>
+  <div class="spotlight module">
+    <span>Block spotlight failed to load, refresh page to try again</span>
   </div>
+  <div class="second module">
+    <div class="second-left second-content">
+      <div class="module-title">
+        <div class="module-title-left">
+          <div class="module-title-left-icon icon iconfont icon-my"></div>
+          <div class="module-title-left-word">Top Validators</div>
+        </div>
+        <div class="module-title-viewall">
+          View All
+        </div>
+      </div>
+      <div class="chart">
+        <highcharts :options="chartOptions" :plotOptions="plotOptions"></highcharts>
+      </div>
+      <div class="empty-data">
+        No validators availabel
+      </div>
+    </div>
+    <div class="second-right second-content">
+      <div class="module-title">
+        <div class="module-title-left">
+          <div class="module-title-left-icon icon iconfont icon-listview"></div>
+          <div class="module-title-left-word">Transaction History</div>
+        </div>
+        <div class="module-title-viewall">
+          View All
+        </div>
+      </div>
+      <div class="select-time">
+        <div class="select-time-all">
+          <el-date-picker v-model="value1" type="daterange" range-separator="" start-placeholder="From" end-placeholder="To" />
+        </div>
+        <div class="select-time-button">
+          <el-button type="primary">Apply</el-button>
+        </div>
+      </div>
+      <div class="empty-data">
+        No transaction availabel
+      </div>
+    </div>
+  </div>
+
+  <div class="second module">
+    <div class="second-left second-content" style="min-height: 60px;">
+      <div class="module-title">
+        <div class="module-title-left">
+          <div class="module-title-left-icon icon iconfont icon-box"></div>
+          <div class="module-title-left-word">Most Recent Blocks</div>
+        </div>
+        <div class="module-title-viewall">
+          View All
+        </div>
+      </div>
+      <!-- <div class="empty-data">
+        No recent block availabel
+      </div> -->
+       <div class="table">
+         <div class="table-item" v-for="(item, index) in 20" :key="index">
+          <div class="table-item-left">
+            <div class="table-item-left-number">48957</div>
+            <div class="table-item-left-transaction">Transactions: 1</div>
+          </div>
+          <div class="table-item-right">
+            <div class="table-item-right-ago">> 12s ago</div>
+            <div class="table-item-right-time">2022/01/11 10:21:07+UTC</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="second-right second-content" style="min-height: 60px;">
+      <div class="module-title">
+        <div class="module-title-left">
+          <div class="module-title-left-icon icon iconfont icon-ic24-transaction"></div>
+          <div class="module-title-left-word">Most Recent Transactions</div>
+        </div>
+        <div class="module-title-viewall">
+          View All
+        </div>
+      </div>
+      <!-- <div class="empty-data">
+        No recent transaction availabel
+      </div> -->
+       <div class="table">
+        <div class="table-item" v-for="(item, index) in 20" :key="index">
+          <div class="table-item-left">
+            <div class="table-item-left-number"> <span class="tx">TX#</span> 0eshaskhe89754kjhaksfh</div>
+            <div class="table-item-left-transaction">Token</div>
+          </div>
+          <div class="table-item-right">
+            <div class="table-item-right-ago">> 12s ago</div>
+            <div class="table-item-right-time">Fee: 0.01 ZOS ZOS</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import NavTab from '@/components/navTab'
+import {
+  Chart
+} from 'highcharts-vue'
+import {
+  datePicker,
+  button
+} from 'element-plus'
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    NavTab,
+    datePicker,
+    button,
+    highcharts: Chart,
+  },
+  data() {
+    return {
+      value1: '',
+      value2: '',
+      currentTab: 0,
+      
+      chartOptions: {
+        type: 'pie',
+        options3d: {
+          enabled: true,
+          alpha: 45,
+          beta: 0
+        },
+        title: {
+          text: 'Top Validators'
+        },
+        tooltip: {
+          pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+        },
+        plotOptions: {
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            depth: 35,
+            dataLabels: {
+              enabled: true,
+              format: '{point.name}'
+            }
+          }
+        },
+        series: [{
+          type: 'pie',
+          name: '占比',
+          data: [
+            ['text1', 45.0],
+            ['text2', 26.8],
+            // {
+            //   name: 'Chrome',
+            //   y: 12.8,
+            //   sliced: true,
+            //   selected: true
+            // },
+            ['text3', 26.8],
+            ['text4', 8.5],
+            ['text5', 6.2],
+            ['text6', 0.7]
+          ]
+        }]
+      },
+    };
+  },
+  methods: {
+    clickTabsItem(currentTab) {
+      this.currentTab = currentTab
+    }
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.home {
+  background: #000;
+  width: 100%;
+  padding-bottom: 100px;
+}
+
+.module {
+  width: 76%;
+  background: #fff;
+  border-radius: 2px;
+  padding: 10px;
+  margin: 0 auto;
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &-title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    &-left {
+      display: flex;
+      justify-content: flex-start;
+      align-items: center;
+
+      &-icon {
+        font-size: 16px;
+        color: #4b81eb;
+      }
+
+      &-word {
+        text-indent: 8px;
+      }
+    }
+
+    &-viewall {
+      font-size: 16px;
+      color: #4b81eb;
+      cursor: pointer;
+    }
+  }
+}
+
+.second {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+
+  &-content {
+    width: 50%;
+    padding: 10px;
+    min-height: 136px;
+  }
+
+  &-left {
+    border-right: 2px solid gray;
+  }
+
+}
+
+.empty-data {
+  margin-top: 20px;
+}
+
+.select-time {
+  margin-top: 20px;
+  display: flex;
+
+  &-button {
+    padding-left: 10px;
+  }
+}
+.table {
+  &-item {
+    padding: 4px 0;
+    border-bottom: 1px solid gainsboro;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;font-size: 12px;
+    &-left {
+      color: #333;
+      flex: 1;
+      &-number {
+        color: #4b81eb;
+      }
+      &-transaction {
+
+      }
+    }
+    &-right {
+      text-align: right;
+      &-ago {
+
+      }
+      &-time {
+
+      }
+    }
+  }
+}
+.tx {
+  color: #333;
+}
+</style>
