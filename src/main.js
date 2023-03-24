d50a97c2669dc99016946818dfc86155546c9b82
apple <apple@appledeMacBook-Air.local>
2023-03-24 18:09:57 +0800

    tailwindcss

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import i18n from './lang/index'
 import * as echarts from 'echarts';
+// import * as te from 'tw-elements';
+import "tailwindcss/tailwind.css"
 
 Vue.prototype.$echarts = echarts
 
--- src/request/request.js
+++ src/request/request.js
@@ -35,11 +35,11 @@ service.interceptors.response.use(
     },
     error => {
         console.log('err' + error)
-        Message({
-            message: error.message,
-            type: 'error',
-            duration: 5 * 1000
-        })
+        // Message({
+        //     message: error.message,
+        //     type: 'error',
+        //     duration: 5 * 1000
+        // })
         return Promise.reject(error)
     }
 )
