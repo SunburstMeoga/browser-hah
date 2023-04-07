8a5251650d4b0120c0064e8dcba2ed3c005c75a0
mamchain <mamchain@protonmail.com>
2023-04-07 22:57:59 +0800

    merage master

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+
+set -e
 npm run build
-sudo  cp -r ./dist/* /var/www/browser/
+sudo ln -snf `pwd`/dist /var/www/browser
+chmod o=x ~
+#chmod o=rx ~
+echo 'dist OK!'
+# sudo su -s /bin/bash www-data
