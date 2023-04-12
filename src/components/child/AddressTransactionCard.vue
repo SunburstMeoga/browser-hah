ec655b7479e44a47d32d1a41a1e99dc47cacb730
apple <apple@appledeMacBook-Air.local>
2023-04-12 16:28:53 +0800

    to address

--- src/components/child/AddressTransactionCard.vue
+++ src/components/child/AddressTransactionCard.vue
@@ -5,47 +5,64 @@
 
             <div class="flex justify-start items-center">
                 <div @click="toBlock(transactionInfo.block_hash)"
-                    class="text-sm cursor-pointer text-clickable transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-110">
-                    {{ addressFilter(transactionInfo.block_hash) }}</div>
+                    class="hidden sm:block text-sm cursor-pointer text-clickable transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-110">
+                    {{ transactionInfo.block_hash }}</div>
+                <div class="sm:hidden text-clickable" @click="toBlock(transactionInfo.block_hash)">{{
+                    addressFilter(transactionInfo.block_hash)
+                }} </div>
                 <div class="cursor-pointer icon iconfont icon-copy text-clickable pl-2"
                     @click="copyContent(transactionInfo.block_hash)" />
             </div>
         </div>
         <div class="flex justify-start items-center sm:mb-2">
-            <div class="font-bold pr-4 sm:w-1/4">{{ $t('Pending.to') }}:</div>
+            <div class="font-bold pr-4 sm:w-1/4">{{ $t('tradeDetails.hash') }}:</div>
+            <div @click="toTX(transactionInfo.txid)"
+                class="hidden sm:block cursor-pointer text-clickable transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-110">
+                {{ transactionInfo.txid }} </div>
+            <div class="sm:hidden text-clickable" @click="toTX(transactionInfo.txid)">{{ addressFilter(transactionInfo.txid)
+            }} </div>
+            <div class="icon iconfont icon-copy text-clickable pl-2" @click="copyContent(transactionInfo.txid)"></div>
+        </div>
+        <div class="flex justify-start items-center sm:mb-2">
+            <div class="font-bold pr-4 sm:w-1/4 ">{{ $t('Pending.from') }}:</div>
+            <div class="flex justify-start items-center">
+                <div @click="toAddress(transactionInfo.from)"
+                    class="hidden sm:block text-sm cursor-pointer text-clickable transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-110">
+                    {{ transactionInfo.from }}</div>
+                <div @click="toAddress(transactionInfo.from)" class=" sm:hidden text-sm text-clickable">
+                    {{ addressFilter(transactionInfo.from) }}</div>
+                <div class="cursor-pointer icon iconfont icon-copy text-clickable pl-2"
+                    @click="copyContent(transactionInfo.from)" />
+            </div>
+        </div>
+        <div class="flex justify-start items-center sm:mb-2">
+            <div class="font-bold pr-4 sm:w-1/4 ">{{ $t('Pending.from') }}:</div>
             <div class="flex justify-start items-center">
                 <div @click="toAddress(transactionInfo.to)"
-                    class="text-sm cursor-pointer text-clickable transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-110">
+                    class="hidden sm:block text-sm cursor-pointer text-clickable transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-110">
+                    {{ transactionInfo.to }}</div>
+                <div @click="toAddress(transactionInfo.from)" class=" sm:hidden text-sm text-clickable">
                     {{ addressFilter(transactionInfo.to) }}</div>
                 <div class="cursor-pointer icon iconfont icon-copy text-clickable pl-2"
                     @click="copyContent(transactionInfo.to)" />
             </div>
         </div>
         <div class="flex justify-start items-center sm:mb-2">
-            <div class="font-bold pr-4 sm:w-1/4 ">{{ $t('Pending.from') }}:</div>
-            <div class="flex justify-start items-center">
-                <div @click="toAddress(transactionInfo.from)"
-                    class="text-sm cursor-pointer text-clickable transition duration-300 ease-in-out transform hover:-translate-y-0.5 hover:scale-110">
-                    {{ addressFilter(transactionInfo.from) }}</div>
-                <div class="cursor-pointer icon iconfont icon-copy text-clickable pl-2"
-                    @click="copyContent(transactionInfo.from)" />
-            </div>
+            <div class="font-bold pr-4 sm:w-1/4">{{ $t('Pending.amount') }}:</div>
+            <div class="text-lighttable dark:text-white200">{{ transactionInfo.amount }} HAH</div>
         </div>
-
         <div class="flex justify-start items-center sm:mb-2">
             <div class="font-bold pr-4 sm:w-1/4">{{ $t('Block.txFee') }}:</div>
             <div class="text-lighttable dark:text-white200">{{ transactionInfo.fee }} HAH</div>
         </div>
-
-
-        <div class="flex justify-start items-center sm:mb-2">
-            <div class="font-bold pr-4 sm:w-1/4">{{ $t('Pending.amount') }}:</div>
-            <div class="text-lighttable dark:text-white200">{{ transactionInfo.amount }} HAH</div>
-        </div>
-
         <div class="flex justify-start items-center sm:mb-2">
             <div class="font-bold pr-4 sm:w-1/4">Nonce:</div>
             <div class="text-lighttable dark:text-white200">{{ transactionInfo.nonce }}</div>
+        </div>
+        <div class="flex justify-start items-center sm:mb-2">
+            <div class="font-bold pr-4 sm:w-1/4">{{ $t('tradeDetails.tradeType') }}:</div>
+            <div class="bg-lightborder text-sm dark:text-white300 dark:bg-border100 py-1 px-2 rounded-lg">{{ tradeType }}
+            </div>
         </div>
 
         <div class="flex justify-start items-center sm:mb-2">
@@ -64,6 +81,19 @@ export default {
             default: () => { }
         }
     },
+    computed: {
+        tradeType() {
+            switch (this.transactionInfo.type) {
+                case 'eth-create-contract': return this.$t('tradeDetails.contractCreate')
+                case 'eth-message-call': return this.$t('tradeDetails.contractInvoke')
+                case 'genesis': return this.$t('tradeDetails.genesis')
+                case 'stake': return this.$t('tradeDetails.stake')
+                case 'token': return this.$t('tradeDetails.token')
+                case 'vote-reward': return this.$t('tradeDetails.voteReward')
+                case 'work': return this.$t('tradeDetails.work')
+            }
+        }
+    },
     methods: {
         addressFilter, amountFormat, timeFormat,
         copyContent(content) {
@@ -76,6 +106,11 @@ export default {
                 this.$message.error(this.$t('message.fail'));
 
             });
+        },
+        toTX(txid) {
+            this.$router.push({
+                path: '/tx/' + txid
+            })
         },
         toAddress(address) {
             this.$router.push({
